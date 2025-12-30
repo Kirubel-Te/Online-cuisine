4480fbaa46b9f758429d878f73732fba835f1093
Kirubel Tesfaye <kirubeltesfaye519@gmail.com>
2025-12-30 13:25:30 +0300

    footer desktop design added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,15 +5,15 @@ import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube } from 'react-icons/fa'
 const Footer = () => {
   return (
     <footer className="bg-amber-950 text-white py-8 mt-auto">
-      <div className="max-w-6xl mx-auto px-4">
-        <div className="flex flex-col gap-8">
+      <div className=" mx-auto px-4 md:grid md:grid-cols-4 md:gap-8 md:w-full">
+        
           <div>
             <div className="flex items-center gap-3">
               <ChefHat className="w-10 h-10" />
               <h2 className="text-xl font-semibold">Foodi Finder</h2>
             </div>
 
-            <div className="mt-5 mb-5 text-amber-200">
+            <div className="mt-5  text-amber-200">
               <p>Discover, cook, and share delicious recipes from around the world. Our mission is to make cooking accessible and enjoyable for everyone.</p>
             </div>
 
@@ -43,7 +43,7 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="quick-access">
+          <div className="quick-access mt-8 md:mt-0">
             <h2 className="text-white text-xl md:text-2xl font-bold mb-3">Quick Links</h2>
             <ul className="space-y-2 ">
               <li>
@@ -61,7 +61,7 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div className="popular-categories">
+          <div className="popular-categories mt-8 md:mt-0">
             <h2 className="text-white text-xl md:text-2xl font-bold mb-3">Popular Categories</h2>
             <ul className="space-y-2">
               <li>
@@ -81,30 +81,30 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-        </div>
+        
 
         
-        <div className='mt-5'>
+        <div className='mt-8 md:mt-0'>
             <h2 className="text-white text-xl md:text-2xl font-bold mb-3">Newsletter</h2>
             <p className='mt-5 mb-5 text-amber-200'>Subscribe to get weekly recipe inspiration and cooking tips!</p>
             <div>
                 <form>
                     <input placeholder='Your email' className='bg-amber-800 w-full h-10 rounded-lg pl-3 mb-4'/>
-                    <button className='bg-amber-500  w-full h-10 rounded-lg text-lg '>Subscribe</button>
+                    <button className='bg-amber-500  w-full h-10 rounded-lg cursor-pointer text-lg '>Subscribe</button>
                 </form>
             </div>
         </div>
       </div>
-      <div className="mt-8">
-        <hr className='text-amber-400 w-3/4 mx-auto mb-8'/>
-        <div className='text-center w-3/5 mx-auto'>
-            <ul className='flex justify-around text-amber-600'>
-                <li><a>Privacy Policy</a></li>
-                <li><a>Terms of Service</a></li>
-                <li><a>Contact Us</a></li>
+      <hr className='text-amber-400 w-3/4 mx-auto mb-8 mt-8'/>
+      <div className=" md:grid md:grid-cols-2 md:justify-between px-4">
+          <p className="text-sm text-amber-600 opacity-80 text-center md:mt-0 md:text-left mt-12">© {new Date().getFullYear()} Foodi Finder. All rights reserved.</p>
+          <div className='policies text-sm  cursor-pointer text-center w-3/5 mx-auto md:mx-0 md:w-auto md:justify-self-end'>
+            <ul className='flex md:gap-8 justify-around  text-amber-600'>
+                <li className='opacity-80 hover:opacity-120'><a>Privacy Policy</a></li>
+                <li className='opacity-80 hover:opacity-120'><a>Terms of Service</a></li>
+                <li className='opacity-80 hover:opacity-120'><a>Contact Us</a></li>
             </ul>
           </div>
-          <p className="text-sm text-amber-600 opacity-80 text-center mt-12">© {new Date().getFullYear()} Foodi Finder. All rights reserved.</p>
           
         </div>
     </footer>
